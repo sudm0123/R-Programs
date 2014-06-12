075214614627b8a22d47bec3c9a0ae6274a8d0d4
Sudhanshu <sudm0123@yahoo.com>
2014-06-13 01:26:44 +0530

    Final Coursera R Programming-Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -9,14 +9,14 @@
 ## 4)get the value of the inverse (get_inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
-	inverse <- NULL
+	inverse <- NULL 					## Initially assigning 'NULL' to inverse
 	set_matrix <- function(y) {			
-		x <<- y
+		x <<- y 					## Setting the matrix 'x'
 		inverse <<- NULL
 	}
-	get_matrix <- function() x
-	set_inverse <- function(solve) inverse <<- solve
-	get_inverse <- function() inverse
+	get_matrix <- function() x 				## Returning matrix 'x'
+	set_inverse <- function(solve) inverse <<- solve 	## Cache the value of the inverse 
+	get_inverse <- function() inverse 			## Returning inverse
 	list(set_matrix = set_matrix, get_matrix = get_matrix,
 	     set_inverse = set_inverse,
 	     get_inverse = get_inverse)
@@ -29,15 +29,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via 
 ## the 'set_inverse' function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-	inverse <- x$get_inverse()
-	if(!is.null(inverse)) {
-		message("getting cached data")
+cacheSolve <- function(x, ...) {				## Return a matrix that is the inverse of 'x'
+	inverse <- x$get_inverse()				## Getting inverse
+	if(!is.null(inverse)) {					## Checking for the presence of inverse
+		message("getting cached data")			## Displaying message
 		return(inverse)
 	}
-	data <- x$get_matrix()
-	inverse <- solve(data, ...)
-	x$set_inverse(inverse)
-	inverse
+	data <- x$get_matrix()					## Getting Matrix
+	inverse <- solve(data, ...)				## Using solve() to compute inverse
+	x$set_inverse(inverse)					## To cache the inverse
+	inverse 						## Returning the inverse
 }
